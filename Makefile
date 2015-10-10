4fa23c7b7cc9ce7bf1d49ba9cee759f2b8e8989f
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-10-10 16:16:01 -0400

    Add Shader Loading
    Added Shader Loading Support and Debug Error Checking Support

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ src/*.cpp -lGL -lGLEW -lglfw -o theperfecterday
+	g++ src/*.cpp -lGL -lGLEW -lglfw -o theperfecterday --std=c++11
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,20 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-GLFWwindow *window;
+#include <iostream>
+#include <fstream>
+#include <string>
 
+GLFWwindow *window;
+GLuint mainShader;
+
+
+// Declare some functions
+
+GLuint loadShader(const char* vertLoc, const char* fragLoc);
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
+                            GLenum severity, GLsizei length,
+                            const GLchar *msg, void *data);
 int main(int argc, char** argv)
 {
     glfwInit();
@@ -18,7 +30,12 @@ int main(int argc, char** argv)
 
     glewInit();
 
-    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(GLDebugMessageCallback, NULL);
+
+    mainShader = loadShader("vert.glsl", "frag.glsl");
+
+    glClearColor(100.0f/ 255.0f, 149.0f/255.0f, 237.0f/255.0f, 1.0f);
     while (!glfwWindowShouldClose(window))
     {
         int count;
@@ -36,4 +53,142 @@ int main(int argc, char** argv)
         glClear(GL_COLOR_BUFFER_BIT);
     }
     return 0;
+}
+
+GLuint loadShader(const char* vertLoc, const char* fragLoc)
+{
+    // Load shader and shit
+    std::ifstream vertIn(vertLoc);
+    std::string vertexStr((std::istreambuf_iterator<char>(vertIn)),
+                          std::istreambuf_iterator<char>());
+
+    const char* vertexChars = vertexStr.c_str();
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexChars, NULL);
+    glCompileShader(vertexShader);
+
+    GLint status;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    printf("Vertex Shader Status %d\n", status);
+
+    std::ifstream fragIn(fragLoc);
+    std::string fragStr((std::istreambuf_iterator<char>(fragIn)),
+                        std::istreambuf_iterator<char>());
+    const char* fragmentChars = fragStr.c_str();
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentChars, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    printf("Fragment Shader Status %d\n", status);
+
+    GLuint shaderProg = glCreateProgram();
+    glAttachShader(shaderProg, vertexShader);
+    glAttachShader(shaderProg, fragmentShader);
+    glLinkProgram(shaderProg);
+
+    glGetShaderiv(shaderProg, GL_LINK_STATUS, &status);
+    printf("Shader Link Status %d\n", status);
+
+    return shaderProg;
+}
+
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
+                            GLenum severity, GLsizei length,
+                            const GLchar *msg, void *data)
+{
+    char* _source;
+    char* _type;
+    char* _severity;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+        _source = "API";
+        break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = "WINDOW SYSTEM";
+        break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = "SHADER COMPILER";
+        break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = "THIRD PARTY";
+        break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+        _source = "APPLICATION";
+        break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+        _source = "UNKNOWN";
+        break;
+
+        default:
+        _source = "UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+        _type = "ERROR";
+        break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = "DEPRECATED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = "UDEFINED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+        _type = "PORTABILITY";
+        break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = "PERFORMANCE";
+        break;
+
+        case GL_DEBUG_TYPE_OTHER:
+        _type = "OTHER";
+        break;
+
+        case GL_DEBUG_TYPE_MARKER:
+        _type = "MARKER";
+        break;
+
+        default:
+        _type = "UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+        _severity = "HIGH";
+        break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        _severity = "MEDIUM";
+        break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+        _severity = "LOW";
+        break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        _severity = "NOTIFICATION";
+        break;
+
+        default:
+        _severity = "UNKNOWN";
+        break;
+    }
+
+    printf("%d: %s of %s severity, raised from %s: %s\n",
+            id, _type, _severity, _source, msg);
 }
